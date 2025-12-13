7f3cf24a65047c85da044f7837295e3d302a5411
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-13 12:57:50 +0100

    damn, react makes life hard

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+interface Todo {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+useEffect(() => {
+  const fetchTodos = async (): Promise<void> => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/')
+      const data = await response.json()
+      setTodos(data)
+    } catch (error) {
+      console.error('Error fetching posts:', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+  fetchTodos();
+}, []);
+
+return (
+  <div>
+    <h1>Todos</h1>
+    {loading ? (
+      <div>Loading...</div>
+    ) : (
+      <ul>
+        {todos.map(todo => 
+          <li key={todo.id}>{todo.title}</li>
+        )}
+      </ul>
+    )}
+  </div>
   )
 }
 
