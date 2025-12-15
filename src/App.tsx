342b1667ad7a3b43e4204f5d549eab758e50e9d7
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-15 04:25:22 +0100

    app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react'
 import type { Todo } from './types'
 import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 import './App.css'
 
 function App() {
